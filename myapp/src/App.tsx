73be66016d54bbae5ae99a0526120250e249bd69
connorjohnson85 <connorjohnson853@gmail.com>
2024-09-16 18:14:06 -0600

    typsecript changes

--- myapp/src/App.tsx
+++ myapp/src/App.tsx
@@ -1,13 +1,17 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [loggedin, setLoggedIn] = useState()
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
